1eb5c8f02dd4d3b3d6642e05533e7569d8286697
sophryu99 <sehwa_ryu@berkeley.edu>
2020-09-13 21:02:08 +0900

    String: 4864 완료

--- SWEA/String_4864_문자열비교.py
+++ SWEA/String_4864_문자열비교.py
@@ -6,11 +6,20 @@
 # [출력]
 # 각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, 답을 출력한다.
 
+""" ---------- 내 풀이 (pass) ---------- """
+# 접근: M안에서 존재할 수 있는 N의 길이와 같은 문자열을 모두 출력하여 저장한 후, N과 비교해서 일치하면 1 리턴.
 
 T = int(input())
 for i in range(T):
     N = input()
-    M = input()
-    res = 0
-    for j in range(len(M - len(str1) + 1)):
-        
+    M = input() 
+    result = []
+    count = 0
+    for l in range(len(M)):             # M입력 길이 안에서의 인덱스 각각 비교
+        result.append(M[l:l+len(N)])    # N입력의 길이만큼 존재하는 String 배열 result에 저장
+    for m in result:                    # result list에서
+        if m == N:                      # 입력 N의 스트링 값과 값이 일치하면
+            count +=1                   # count 증감
+    print("#%d %d" %(i+1,count))
+    
+
