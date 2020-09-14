e73262af71ad024e1c99db470ef23c42600ae1c9
sophryu99 <sehwa_ryu@berkeley.edu>
2020-09-14 18:20:42 +0900

    String: 4861 -ing

--- SWEA/String_4861_palindrome.py
+++ SWEA/String_4861_palindrome.py
@@ -8,7 +8,7 @@
 # N : N x N 글자판
 # M: 회문의 길이
 
-""" ---------- 내 풀이 (pass) ---------- """
+""" ---------- 내 풀이 () ---------- """
 
 # 접근: 한 줄 안에서 M개의 길이로 존재할 수 있는 스트링을 새로운 배열에 저장해준 후 모든 배열들을 뒤집어 보고 palidrome 이 맞으면 해당 string 값을 리턴한다
 # 가로 후 세로 순서로 진행한다
@@ -16,9 +16,9 @@
 T = int(input())
 for i in range(T):
     N, M = map(int,input().split())
-    horizontal_res = []                         # 1. 가로 먼저 탐색
+    horizontal_res = []                        
     for n in range(N):
-        lst_h = input()
+        lst_h = input()                         # 1. 가로 먼저 탐색
         result_h = []                           # 입력받는 리스트 N에서 M개의 길이인 스트링값 저장하는 변수
         for m in range(len(lst_h)):
             result_h.append(lst_h[m:m+M])       # 입력받는 리스트 범위 내에서 M의 길이만큼의 배열을 result_h에 저장한다
@@ -27,6 +27,15 @@ for i in range(T):
                 if r == r[::-1]:                # 뒤집었을 때 원래의 값과 같은 값에 한해서
                     horizontal_res.append(r)    # horizontal_res에 저장해준다.
     
+    vertical_res = []                           # 2. 세로 탐색 시작
+    vertical_list = []                          # N번 입력 리스트를 받으며 새로 생성하는 리스트 변수
+    for x in range(N):
+        lst_v = input()
+        vertical_list.append(lst_v[x])
+    
+
+
+
 
 
         
