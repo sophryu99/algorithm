765c60a29338f3bcf805e34a44112a331a2a2816
sophryu99 <sehwa_ryu@berkeley.edu>
2021-03-07 16:20:47 +0900

    update docs

---
+++ docs/bfs_pseudocode.py
@@ -0,0 +1,17 @@
+# create grid 2-D array
+# create visited list
+
+# def bfs(x, y):
+#     create queue
+#     append(x, y) to queue
+#     append(x, y) to visited
+
+#     while queue exists:
+#         pop first (x, y) from queue
+#         relocate newX index
+#         relocate newY index
+#         if (newX, newY) not in visited and within the range(len(grid)) and is a path:
+#             append(newX, newY) to queue
+#             append(newX, newY) to visited
+#             if grid[newX][newY] == end:
+#                 return True
