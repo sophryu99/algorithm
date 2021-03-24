587ab093ed50d8c82d97b3782385ac406692224e
sophryu99 <sehwa_ryu@berkeley.edu>
2021-03-24 18:57:03 +0900

    9935: ing

---
+++ BaekJoon/9935_문자열폭발.py
@@ -0,0 +1,13 @@
+
+word = list(input())  
+bomb = list(input())
+
+stck = []
+for i in range(len(word)):
+    for j in range(len(bomb)):
+        if word[i] == bomb[j]:
+            print('')
+
+
+
+
