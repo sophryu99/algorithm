a6a572c29549f8b54452565f0c47c39a7264ab78
sophryu99 <sehwa_ryu@berkeley.edu>
2021-04-01 16:36:22 +0900

    1166: binary search done

---
+++ BaekJoon/1166_선물.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/1166
+
+import sys
+
+N, L, W, H = map(int, sys.stdin.readline().split())
+S, E = 0, max(L, W, H)
+
+for _ in range(10000):
+    M = (S + E) / 2
+    count = (L // M) * (W // M) * (H // M)
+    if count >= N:
+        S = M
+    else:
+        E = M
+
+print("%.10f" %(E))
+
+
