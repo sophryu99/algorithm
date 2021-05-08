e4682316cf5ce47477bc91768969cdd810926f80
sophryu99 <sehwa_ryu@berkeley.edu>
2021-05-08 16:30:11 +0900

    k번째수: done

---
+++ Programmers/k번째수.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42748?language=python3
+
+def solution(array, commands):
+    # i, j, kth
+    answer = []
+    for i in commands:
+        newarr = sorted(array[i[0] - 1 : i[1]])
+        answer.append(newarr[i[2]-1])
+    return answer
+
