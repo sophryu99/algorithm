0ba7d9e878d479549a8e2dd91298d7b198b47406
sophryu99 <sehwa_ryu@berkeley.edu>
2021-09-25 16:43:30 -0700

    9/25

---
+++ leetcode/22_generate_parenthesis.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/generate-parentheses/
+
+# Backtracking
+"""
+1. If we still have one left to place, we will start an opening bracket
+2. Start a closing bracket if it would not exceed the number of opening brackets
+"""
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        pairs = []
+        
+        def backtrack(s = [], left = 0, right = 0):
+            
+            if len(s) == 2 * n:
+                pairs.append("".join(s))
+                return
+            
+            if left < n: 
+                s.append("(")
+                backtrack(s, left + 1, right)
+                s.pop()
+                
+            if right < left:
+                s.append(")")
+                backtrack(s, left, right + 1)
+                s.pop()
+            
+        backtrack()
+        return pairs
+        
+            
+            
+        
+        
+        
+        
+        
+        
+        
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,14 +1,18 @@
 from typing import Dict
 
 
-dict = {3: 1, 4: 2}
-print(dict)
+# dict = {3: 1, 4: 2}
+# print(dict)
 
-dict[3] = 4
-print(Dict)
+# dict[3] = 4
+# print(Dict)
 
-if 3 in dict:
-    print (dict[3])
+# if 3 in dict:
+#     print (dict[3])
 
-dict.pop(3)
-print(dict)
+# dict.pop(3)
+# print(dict)
+
+nums = [1,2,3,4,5,7]
+for i, n in enumerate(nums):
+    print(i, n)
