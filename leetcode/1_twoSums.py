1d408191441a48fa9bebaebe92b37ade31d6deb9
sophryu99 <sehwa_ryu@berkeley.edu>
2021-10-19 21:43:29 -0700

    10/19

--- leetcode/1_twoSums.py
+++ leetcode/1_twoSums.py
@@ -11,7 +11,23 @@ class Solution:
                         break
 
 """
+TLE
 Runtime: 48 ms, faster than 63.01% of Python3 online submissions for Two Sum.
 Memory Usage: 14.3 MB, less than 75.07% of Python3 online submissions for Two Sum.
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in range(len(nums)): 
+            if target - nums[i] in nums[i + 1:]:
+                n = nums[i + 1:]
+                return [i, n.index(target - nums[i]) + len(nums) - len(n)]
+    
+    
+            
+"""
+Runtime: 652 ms, faster than 36.11% of Python3 online submissions for Two Sum.
+Memory Usage: 14.9 MB, less than 65.96% of Python3 online submissions for Two Sum.
 """
                         
