b4a093770db606d2282fc66eeded8a6aea4ddd73
sophryu99 <sehwa_ryu@berkeley.edu>
2022-03-06 17:34:22 -0800

    3/6

---
+++ leetcode/27_remove_element.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/remove-element/
+
+class Solution:
+    def removeElement(self, nums, val):
+        i = 0
+        for x in nums:
+            if x != val:
+                nums[i] = x
+                i += 1
+        return i
+
